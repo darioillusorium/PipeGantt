c16985acb85d4e06487f4b2c6559f1529ad6c855
drodriguez <drodriguez@illusoriumstudios.com>
2024-05-27 14:31:39 +0200

    More info error

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ export default class Gantt {
     } else {
       throw new TypeError(
         "Frappé Gantt only supports usage of a string CSS selector," +
-        " HTML DOM element or SVG DOM element for the 'element' parameter",
+        " HTML DOM element or SVG DOM element for the 'element' parameter. Current type: " + typeof element,
       );
     }
 
